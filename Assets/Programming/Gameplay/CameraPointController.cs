e4d03fe6c610464515274c7f4739765ce8890cb3
savasva <savasva@mail.ru>
2023-10-24 20:37:02 -0700

    Added Simple Cameras

---
+++ Assets/Programming/Gameplay/CameraPointController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPointController : MonoBehaviour //TODO it should be an object from the object space
+{
+    public Vector2 RangeHorizontal;
+    public Vector2 RangeVertical;
+
+    public void CamRotate(Vector2 direction) 
+    {
+
+    }
+}
