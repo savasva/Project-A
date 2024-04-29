92f8c57da4867b3c676bde34a5621a1cc5629630
MaxWhite2024 <mjwhite@scu.edu>
2024-04-29 11:56:50 -0700

    basic engr chat

---
+++ Assets/Programming/Scripts/UI/ChatManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+using TMPro;
+using System.IO;
+
+public class ChatManager : MonoBehaviour
+{
+    //Message Text Field
+    [SerializeField] private TMP_Text MessageTextComponent;
+
+    public void SelectEngrChat()
+    {
+        //set the text shown in the text component to the engineer's chat history
+        //MessageTextComponent.text = ;
+    }
+
+    public void SelectBioChat()
+    {
+
+    }
+}
