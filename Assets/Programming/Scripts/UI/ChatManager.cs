b88cfb85d56f489dd025d6dcbc673561b80d64e6
MaxWhite2024 <mjwhite@scu.edu>
2024-04-29 18:36:07 -0700

    empty chatmanager script

--- Assets/Programming/Scripts/UI/ChatManager.cs
+++ Assets/Programming/Scripts/UI/ChatManager.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 //using UnityEngine.UI;
 using TMPro;
 using System.IO;
+using System.Linq;
 
 public class ChatManager : MonoBehaviour
 {
     //Message Text Field
     [SerializeField] private TMP_Text MessageTextComponent;
+
+    //[SerializeField] private GameObject recalTextObject;
+    private string txt;
+
+    void Start()
+    {
+        //get engr chat log
+        //...
+
+        //get bio chat log
+        //...
+
+        //store all lines of file in fileLines
+        //string fileLines = File.ReadAllText(txt);
+        //List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
+
+        //
+    }
 
     public void SelectEngrChat()
     {
